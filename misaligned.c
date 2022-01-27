e4fa67b8b9ee70278df2b96df58db0d22bac9bb5
Vigneshwaran-Gurusamy <91051117+Vigneshwaran-Gurusamy@users.noreply.github.com>
2022-01-27 18:19:53 +0530

    Commit

--- misaligned.c
+++ misaligned.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <assert.h>
 
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+typedef struct {
+    const char* majorColor;
+    const char* minorColor;
+} ColorPair;
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    colorPair.majorColor = MajorColorNames[(pairNumber / numberOfMinorColors)];
+    colorPair.minorColor = MinorColorNames[(pairNumber % numberOfMinorColors)];
+    return colorPair;
+}
+
+void testNumberToPair(int pairNumber, const char* expectedMajor, const char* expectedMinor){
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
@@ -8,6 +32,7 @@ int printColorMap() {
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            testNumberToPair( i * 5 + j, majorColor[i], minorColor[i]);
         }
     }
     return i * j;
